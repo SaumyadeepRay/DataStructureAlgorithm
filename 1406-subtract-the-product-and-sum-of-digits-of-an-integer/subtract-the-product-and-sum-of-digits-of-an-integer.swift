8355d3dd0e122c0e2ca0b505280523948f1af3c1
Saumyadeep Ray <88551711+SaumyadeepRay@users.noreply.github.com>
2026-08-10 13:39:12 +0530

    Update subtract-the-product-and-sum-of-digits-of-an-integer.swift

--- 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.swift
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.swift
@@ -1,6 +1,7 @@
 class Solution {
     func subtractProductAndSum(_ n: Int) -> Int {
-        if(n==0) {
+        // If the number is 0, both product and sum are 0
+        if n == 0 {
             return n
         }
 
@@ -8,13 +9,21 @@ class Solution {
         var product = 1
         var sum = 0
 
-        while (temp != 0) {
-            var remainder = temp % 10
+        while temp != 0 {
+            // Get the last digit of the number
+            let remainder = temp % 10
+
+            // Multiply the digit with the current product
             product = product * remainder
+
+            // Add the digit to the current sum
             sum = sum + remainder
+
+            // Remove the last digit from the number
             temp = temp / 10
         }
 
-        return (product - sum)
+        // Return product of digits - sum of digits
+        return product - sum
     }
 }
